3f024443d2d4a0b6a256136d94ba65b038686c2a
bezmari19 <marina.bezerraf19@gmail.com>
2025-07-10 14:29:00 -0300

    :books:docs: usando loop for para contar de 1 a 100

---
+++ usandoFor.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    for (int numeros = 1; numeros < 101; numeros++) { // Loop de 1 a 100
+        printf("%d\n", numeros); // Imprime o número atual
+    }
+    
+    return 0; // Retorna 0 para indicar que o programa terminou corretamente
+}
