ad8244282bcf66625f723ea25c52aa37f948dea7
bezmari19 <marina.bezerraf19@gmail.com>
2025-07-10 13:28:25 -0300

    :bug::books: fix:docs: melhorando o uso de if/else

--- adivinhação/programaAula.c
+++ adivinhação/programaAula.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define NUMERO_TENTATIVAS 3 // Define o número de tentativas permitidas 
 
 int main() {
     // Imprime o cabeçalho do jogo
@@ -8,17 +9,21 @@ int main() {
 
     int numerosecreto = 42; // Variável que armazena o número secreto
     int chute; // Variável que armazena o chute do usuário
+    // int numeroTentativas = 3; // Variável que define o número de tentativas permitidas
 
     //loop 
-    for(int i = 1; i <= 3; i++) // Loop que permite ao usuário fazer 3 tentativas de um em um 
+    for(int i = 1; i <= NUMERO_TENTATIVAS; i++) // Loop que permite ao usuário fazer 3 tentativas de um em um 
     {
-        printf("Tentativa %d de 3\n", i); // Imprime o número da tentativa atual
+        printf("Tentativa %d de %d\n", i, NUMERO_TENTATIVAS); // Imprime o número da tentativa atual
         printf("Qual é o seu chute?\n"); // Solicita o chute do usuário
         
         scanf("%d", &chute); // Lê o chute do usuário
         printf("Seu chute foi: %d\n", chute); // Imprime o chute do usuário
 
         int acertou = (chute == numerosecreto); // Verifica se o chute é igual ao número secreto
+        int maior = (chute > numerosecreto); // Verifica se o chute é maior que o número secreto
+        int menor =(chute < numerosecreto); // Verifica se o chute é menor que o número secreto
+        
         // se o chute for igual ao numero secreto
         //if (chute == numerosecreto)
         if (acertou) {
@@ -26,17 +31,16 @@ int main() {
             break; // Sai do loop se o usuário acertar
         }
         // caso contrário
-        else {
-            
-            int maior = (chute > numerosecreto); //
+        // else {}
+
             //if (chute > numerosecreto) 
-            if (maior) {
-                printf("Seu chute foi maior que o número secreto.\n");
-            } else { // caso contrário if (chute < numerosecreto) 
-                printf("Seu chute foi menor que o número secreto.\n");
-            }
-            // printf("Você errou! Tente novamente.\n");
+        else if (maior) {
+            printf("Seu chute foi maior que o número secreto.\n");
         }
+        else { // caso contrário if (chute < numerosecreto) 
+            printf("Seu chute foi menor que o número secreto.\n");
+        }
+        // printf("Você errou! Tente novamente.\n");
     }
     printf("Fim do jogo!\n"); // imprime a mensagem de fim do jogo quando o usuário acerta ou esgotam as tentativas
 }
